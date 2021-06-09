14546b6cdc3712b73a9c6ed131aaa743fbb5eebe
Michael Anoprenko <michael.anoprenko@aimtech.com>
2021-06-09 17:57:11 +0300

    wip

---
+++ src/len_hist.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(dplyr)
+# library(tidyr)   # replace_na
+# library(tibble)  # column_to_rownames
+
+###
+
+NAME <- 'H3K27me3_H1.ENCFF152JSZ.hg19'
+OUT_DIR <- 'results/'
+
+###
+
+bed_df <- read.delim(paste0('data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+# hist(bed_df$len)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
+
