432062b3c2fc828cb7ed06b7e3f4f3e82f85294c
Michael Anoprenko <michael.anoprenko@aimtech.com>
2021-06-09 18:56:01 +0300

    wip

--- src/len_hist.R
+++ src/len_hist.R
@@ -1,21 +1,15 @@
-library(ggplot2)
-library(dplyr)
-# library(tidyr)   # replace_na
-# library(tibble)  # column_to_rownames
+source('lib.R')
 
 ###
 
-NAME <- 'H3K27me3_H1.ENCFF152JSZ.hg19'
-OUT_DIR <- 'results/'
+NAME <- 'DeepZ'
 
 ###
 
-bed_df <- read.delim(paste0('data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
-head(bed_df)
-
-# hist(bed_df$len)
 
 ggplot(bed_df) +
   aes(x = len) +
@@ -23,4 +17,3 @@ ggplot(bed_df) +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
 ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
-
